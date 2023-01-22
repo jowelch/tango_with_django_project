e2c8bf12353e0e5c1e0a440648928915aedb1399
Joanne Welch <jowelch03@gmail.com>
2023-01-22 20:10:38 +0000

    created index and about pages with templates

--- rango/urls.py
+++ rango/urls.py
@@ -4,7 +4,8 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    # path(<string to match>, <view to call>, <name, convenient way to reference view, optional>)
+    # path(<string to match>, <view to call>, <name, way to reference view, optional>)
     path('',views.index, name='index'),
+    path('about/',views.about, name='about')
 ]
 
